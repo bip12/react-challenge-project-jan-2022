58bc1a613a41889361890b6759d1da93ca895f33
bip12 <bipskarki@gmail.com>
2022-01-09 09:40:09 -0800

    fix(authReducer): Retrieve user from Redux, work #4

--- application/src/redux/reducers/authReducer.js
+++ application/src/redux/reducers/authReducer.js
@@ -1,11 +1,12 @@
 import { LOGIN, LOGOUT } from '../actions/types'
 
-const INITIAL_STATE = { email: null, token: null };
+const INITIAL_STATE = { email: '', token: null };
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case LOGIN:
-            return { ...state, email: action.payload.login, token: action.payload.token }
+            return { ...state, email: action.payload.email, token: action.payload.token } //payload.login -> payload.email
+
         case LOGOUT:
             return { ...state, ...INITIAL_STATE }
         default:
